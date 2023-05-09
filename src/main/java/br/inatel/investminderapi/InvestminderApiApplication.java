50c1e6182587dab8ec3a82563cee2187f7990a5e
RedfoxGHS <gabrielhenri0012@gmail.com>
2023-05-09 06:50:45 -0300

    init

---
+++ src/main/java/br/inatel/investminderapi/InvestminderApiApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.investminderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestminderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestminderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/inatel/investminderapi/InvestminderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.investminderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestminderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
